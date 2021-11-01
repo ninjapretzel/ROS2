d2be62d228877416af57bbcc40cd8035956a271d
ninjapretzel <ninjapretzel@yahoo.com>
2021-11-01 10:07:23 -0600

    Changes training procedur.

--- Assets/Scripts/Util/WallFollowTrainer.cs
+++ Assets/Scripts/Util/WallFollowTrainer.cs
@@ -18,31 +18,52 @@ public class WallFollowTrainer : MonoBehaviour {
 	public class QTData {
 		public float score;
 		public int generation;
+		public int species; 
 		public int trial;
 		public Dictionary<string, float[]> data;
 	}
 
+	[Header("Setup")]
 	public WallFollow wallFollow;
 	public Dictionary<string, float[]> best;
 	public bool sim = false;
+	public bool resimulate = false;
+	public float simDelay = .1f;
 	public Vector3 initialPosition;
 	[Range(0, 360)] public float initialRotation = 0;
+
+	[Header("Save/Load/Tracking")]
 	public string filename = "qt";
 	public int generation = 0;
+	public int species = 0;
 	public int trial = 0;
+	public float simTime = 0;
 	public float bestScore = 0;
 	public float currentScore;
-	public float mutateChance = .01f;
-	public float drift = .1f;
-	public float noScoreTimeout = 30f;
-	public float noScore = 0;
 	public float gain;
 
-	public float[] rightDistanceMults = new float[] { 1f, 10f, 2f, 1f, 0f };
+	[Header("Scoring Settings")]
+	public float[] rightDistanceMults = new float[] { 1f, 2f, 1.5f, 1f, 0f, 0f, 0f, 0f, 0f }; 
+	// public float[] rightDistanceMults = new float[] { 1f, 10f, 2f, 1f, 0f, 0f, 0f, 0f, 0f }; // Note: over-valuing distances is bad.
 	public float forwardScoreMult = 10f;
 	public float maxForwardScore = 5f;
+	
+	[Header("Mutation Settings")]
+	public float mutateChance = .01f;
+	public float drift = .1f;
+	public float driftChance = .9f;
 
-	public float simDelay = .1f;
+	[Header("Damage Settings ")]
+	public float maxHealth = 30f;
+	public float curHealth = 0;
+	public float heal = 1.0f;
+	public float healShrink = 1000;
+	public float damage = 1.0f;
+	public float damageGrow = 180;
+	[SerializeField] private float damageRate = 1.0f;
+	[SerializeField] private float healRate = 1.0f;
+
+
 
 	public static T[] Duplicate<T>(T[] ts) {
 		T[] copy = new T[ts.Length];
@@ -65,6 +86,7 @@ public class WallFollowTrainer : MonoBehaviour {
 				best = qtdata.data;
 				bestScore = qtdata.score;
 				generation = qtdata.generation;
+				species = qtdata.species;
 				trial = qtdata.trial;
 
 				Debug.Log($"Initialized best qt table from {filename}.json: {best.Count} states.");
@@ -82,10 +104,14 @@ public class WallFollowTrainer : MonoBehaviour {
 
 	void OnDisable() {
 		if (best != null) {
-			QTData qt = new QTData() { data = best, score = bestScore, trial = trial, generation = generation };
-			File.WriteAllText(filename + ".json", Json.Reflect(qt).ToString());
-			Debug.Log($"Saved best qt to {filename}.json");
+			SaveBest();
 		}
+	}
+
+	private void SaveBest() {
+		QTData qt = new QTData() { data = best, score = bestScore, generation = generation, species = species, trial = trial, };
+		File.WriteAllText(filename + ".json", Json.Reflect(qt).ToString());
+		Debug.Log($"Saved best qt to {filename}.json");
 	}
 
 	void LateUpdate() {
@@ -94,6 +120,7 @@ public class WallFollowTrainer : MonoBehaviour {
 
 		// Objective function:
 		gain = 1;
+		simTime += Time.deltaTime;
 		int rightState = wallFollow.currentState[3];
 		float forwardSpeed = wallFollow.lastActions[0] - wallFollow.lastActions[1];
 
@@ -102,20 +129,22 @@ public class WallFollowTrainer : MonoBehaviour {
 		float gainA = rightDistanceMults[rightState];
 		// relatively square the gain so that many more points are gained when moving near full speed,
 		// and fewer gained when not moving close to full speed
-		float gainB = Mathf.Lerp(0, forwardScoreMult, Mathf.Clamp01(forwardSpeed));
+		float gainB = Mathf.Lerp(0, forwardScoreMult, forwardSpeed);
 		gainB /= forwardScoreMult;
-		gainB *= gainB;
+		gainB *= gainB * gainB; // cubed to allow negative scores for reverse
 		gainB *= forwardScoreMult;
 
 		gain *= gainA * gainB;
 		float sc = currentScore;
 		currentScore += gain * Time.deltaTime;
+		damageRate = damage + simTime / damageGrow;
+		healRate = heal * (healShrink / (healShrink + simTime));
 
 		if (!(currentScore > sc)) {
-			noScore += Time.deltaTime; 
+			curHealth -= Time.deltaTime * damageRate;
 		} else {
-			noScore -= Time.deltaTime;
-			if (noScore < 0) { noScore = 0; }
+			curHealth += Time.deltaTime * healRate;
+			if (curHealth >= maxHealth) { curHealth = maxHealth; }
 		}
 		
 		if ((wallFollow.lastFlags & CollisionFlags.Sides) != CollisionFlags.None) {
@@ -123,8 +152,8 @@ public class WallFollowTrainer : MonoBehaviour {
 			EndSim();
 			return;
 		}
-		if (noScore > noScoreTimeout) {
-			Debug.Log("Terminating sim - Too long since score gained");
+		if (curHealth <= 0) {
+			Debug.Log("Terminating sim - Not gaining enough score");
 			EndSim();
 			return;
 		}
@@ -135,12 +164,20 @@ public class WallFollowTrainer : MonoBehaviour {
 	void PrepareSim() {
 		wallFollow.transform.position = wallFollow.pos = initialPosition;
 		wallFollow.transform.rotation = wallFollow.rot = Quaternion.Euler(0, initialRotation, 0);
-		trial++;
+		
 		Debug.Log($"Setting up trial {trial} @ generation {generation}");
 		//Debug.Log($"Teleported robot to {wallFollow.transform.position} / {wallFollow.transform.rotation}");
-		wallFollow.qt = Mutate(best);
+		if (resimulate) {
+			resimulate = false;
+			wallFollow.qt = best;
+			bestScore = 0;
+		} else {
+			wallFollow.qt = Mutate(best); 
+			species++;
+		}
 		currentScore = 0;
-		noScore = 0;
+		curHealth = maxHealth;
+		simTime = 0;
 		gain = 0;
 		wallFollow.Reset();
 		
@@ -153,6 +190,7 @@ public class WallFollowTrainer : MonoBehaviour {
 
 	void EndSim() {
 		sim = false;
+		trial++;
 		wallFollow.gameObject.SetActive(false);
 
 		if (currentScore  >= bestScore - Time.deltaTime * gain) {
@@ -160,15 +198,13 @@ public class WallFollowTrainer : MonoBehaviour {
 			best = wallFollow.qt;
 			bestScore = currentScore;
 			generation++;
+			species = 0;
+			SaveBest();
 		}
 		
 		PrepareSim();
 	}
-		
-
-
-	const int DRIFT = 0;
-	const int SET = 1;
+	
 	Dictionary<string, float[]> Mutate(Dictionary<string, float[]> d) {
 		
 		var copy = Copy(d);
@@ -179,13 +215,11 @@ public class WallFollowTrainer : MonoBehaviour {
 				
 				if (Random.value < mutateChance) {
 					
-					int mutateKind = Random.Range(0, 2);
-					if (mutateKind == DRIFT) { // drift action by some small amount
+					if (Random.value < driftChance) { // drift action by some small amount
 						float roll = Random.value;
 						float change = (-1 + roll * 2) * drift;
 						vals[i] = Mathf.Clamp01(vals[i] + drift);
-
-					} else if (mutateKind == SET) { // set action to some random value
+					} else { // set action to some random value
 						vals[i] = Random.value;
 					}
 
